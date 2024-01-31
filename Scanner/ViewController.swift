782203027d8fca7d5e219037453157a3c3c84c85
snowy-summer <118453865+snowy-summer@users.noreply.github.com>
2024-01-31 14:37:05 +0900

    Initial Commit

---
+++ Scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scanner
+//
+//  Created by 최승범 on 2024/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
